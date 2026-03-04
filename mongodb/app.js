a4289998e280e9b590fefab64f9a23c5054c3dd0
NaVteJSingH18 <navtej.dgc@gmail.com>
2026-03-05 00:15:02 +0530

    added btu will change

---
+++ mongodb/app.js
@@ -0,0 +1,46 @@
+const express=require("express")
+
+const mongoose=require("mongoose")
+const userModel= require('./usermodel')
+const app=express();
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('this is home page')
+})
+
+app.get('/create', async (req,res)=>{
+    let createduser= await userModel.create({
+    name:'navtej',
+    email:"navtej@gmail.com",
+    username:"navtej"   
+})
+res.send(createduser);
+})
+app.get('/update', async (req,res)=>{
+   let updateduser = await userModel.findOneAndUpdate(
+       { username:"navtej" },
+       { name:"navi", email:"navi@gmail.com" },
+       { returnDocument:"after" }
+   )
+
+   console.log(updateduser)
+   res.send(updateduser);
+})
+
+app.get('/read',async (req,res)=>{
+    let users=  await userModel.find();
+    res.send(users)
+})
+
+app.get('/delete',async (req,res)=>{
+    let users=  await userModel.findOneAndDelete(
+        {uername:"navtej"}
+    );
+    res.send(users)
+})
+
+app.listen(8000,()=>{
+    console.log("server is running rn")
+})
