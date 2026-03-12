56c9fc0519e88fd0fcf3ececef6491448d43d662
NaVteJSingH18 <navtej.dgc@gmail.com>
2026-03-13 00:18:15 +0530

    dataasso

---
+++ dataasso/app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const mongoose =require("mongoose")
+const app =express();
+const userModel =require("./models/user")
+const postModel = require("./models/post");
+
+
+app.get("/",(req,res)=>{
+    res.send("hi")
+})
+
+app.get("/create" , async (req,res)=>{
+    let user = await userModel.create({
+        username:"navtej",
+        email:"navtej@gmail.com",
+        age:21
+    })
+    res.send(user)
+})
+
+app.get("/post/create" , async (req,res)=>{
+   let post = await postModel.create({
+        postdata:"seedhe maut jal aur jeevan dono",
+        user:"69b304da089c18ec4ee927d8",
+    })
+   let user= await userModel.findOne({_id:"69b304da089c18ec4ee927d8"});
+    user.posts.push(post._id);
+    await user.save();
+    res.send({post,user})
+})
+app.listen(8000, ()=>{"hey This is port 8000"})
